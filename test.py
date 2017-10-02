d1cc579dd201b7a3e0d89d1435ff5f35cd2a4ff8
Alicia Daurignac <>
2017-10-02 17:41:30 +0100

    Updated gitignore

--- test.py
+++ test.py
@@ -8,7 +8,7 @@ im = Image.open("adam.jpg")
 
 one_image = numpy.arange(16).reshape((4, 4))  # create an image as a 2-dimensional array of 16 pixels
 
-#  print(one_image)
+# print(one_image)
 
 patches = image.extract_patches_2d(im, (2, 2), 4) #b reaks up the image into 4 2-dimensional arrays of 4
 print(patches)
