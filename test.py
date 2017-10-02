c1a9755c858618a25f0aa8396fd8521ee7fe3d9f
Alicia Daurignac <>
2017-10-02 18:27:08 +0100

    Tried to split image into patches - not working properly

--- test.py
+++ test.py
@@ -1,14 +1,19 @@
+import PIL
 import cv2 as opencv
 import sklearn
+import matplotlib
 import numpy
 from sklearn.feature_extraction import image
-from PIL import Image
+from skimage.io import imread
+from skimage import io
 
-im = Image.open("adam.jpg")
+#print(io.available_plugins)
+io.use_plugin('pil')  # sets the skimage library to use PIL to read and write images
 
-one_image = numpy.arange(16).reshape((4, 4))  # create an image as a 2-dimensional array of 16 pixels
+im = imread("adam.jpg")
 
-# print(one_image)
+# one_image = numpy.arange(16).reshape((4, 4))  # create an image as a 2-dimensional array of 16 pixels
 
-patches = image.extract_patches_2d(im, (2, 2), 4) #b reaks up the image into 4 2-dimensional arrays of 4
-print(patches)
+patches = image.extract_patches_2d(im, (486, 486),4)  # breaks up the image into 4 2-dimensional arrays of 4
+
+io.imsave("adampatch.jpg", patches[0])  # saves the patch 1 out of 4
