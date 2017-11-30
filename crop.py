f98dfd4918133b5672b89b76cb57a9a93d9efae8
Alicia D <amd223@exeter.ac.uk>
2017-11-30 01:27:14 +0000

    Update crop.py to dump pics in a directory

--- crop.py
+++ crop.py
@@ -1,17 +1,20 @@
+import os
 import cv2
 
-width_blocks = 5
-height_blocks = 5
+curr_dir   = os.path.dirname(os.path.realpath(__file__))
+target_dir = os.path.join(curr_dir, 'w')
+os.makedirs(target_dir, exist_ok=True)   # mkdir -p
+
+width_blocks  = 486
+height_blocks = 486
 
 img = cv2.imread("adam.jpg")
 height, width = img.shape[:2]
 
-block_height = height/height_blocks
-block_width = width/width_blocks
-
-
-for row in range(height_blocks):
-    for column in range(width_blocks):
-        start_x = column * block_width
-        start_y = row * block_height
-        crop = img[start_x, start_y, block_width, block_height]
+# Iterate in the range(begin, end, step)
+for y in range(0, height, height_blocks):
+    for x in range(0, width, width_blocks):
+        crop_name = os.path.join(target_dir, 'img_y-%d_x-%d.png' % (y, x))
+        crop_img  = img[y:y+height_blocks, x:x+width_blocks]
+        print('Creating %s...' % crop_name)
+        cv2.imwrite(crop_name, crop_img)
