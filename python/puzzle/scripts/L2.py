50242f6129614c47a09c078e69ccbc204c090478
Louis Blin <louis.blin14@imperial.ac.uk>
2018-01-24 23:41:13 +0000

    Cleaner L2.py

--- python/puzzle/scripts/L2.py
+++ python/puzzle/scripts/L2.py
@@ -1,7 +1,8 @@
 import numpy as np
 
-from puzzle.tools.utils import input_image, img_read
 from puzzle.tools.crop import crop
+from puzzle.tools.utils import input_image
+
 
 def l2_distance(img, template):
     """
@@ -13,28 +14,21 @@ def l2_distance(img, template):
 
     return distances.sum()
 
+
 img_path = input_image("Input an image name from '{}': \n")
 print('Using image src : ' + img_path)
 
-pieces = crop(img_path,(16,16))
+pieces = crop(img_path, (16, 16))
 
 l2_list = []
 l2_names = []
-k = 0
-i = 0
 
-while i < len(pieces):
-    while k < len(pieces):
+for i in range(len(pieces)):
+    for k in range(len(pieces)):
         distance = l2_distance(pieces[i], pieces[k])
         if distance == 0:
-            l2_names. append((i,k))
+            l2_names.append((i, k))
         l2_list.append(distance)
-        k += 1
-    k = 0
-    i += 1
 
-#return the pairs of pieces which were exact matches
+# return the pairs of pieces which were exact matches
 print(l2_names)
-
-
-
