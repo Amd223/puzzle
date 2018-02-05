21a4bd33505bf43de2e73cc69b232656fc38627a
Alicia D <amd223@exeter.ac.uk>
2018-02-05 11:02:30 +0000

    systematic merging of all images in a directory

--- python/puzzle/scripts/merge_images.py
+++ python/puzzle/scripts/merge_images.py
@@ -13,10 +13,10 @@ rel_path         = '../../../images'
 rel_path_resized = '../../../images/resized'
 rel_path_merged  = '../../../images/merged'
 
-resize_all(mkpath(rel_path), size=200)
+#resize_all(mkpath(rel_path), size=200)
 
-img_path_1 = input_image(images_relpath=rel_path_resized)
-img_path_2 = input_image(images_relpath=rel_path_resized)
+img_path_1 = input_image(images_relpath=rel_path)
+img_path_2 = input_image(images_relpath=rel_path)
 
 # Load image
 img1 = img_read(img_path_1)
@@ -39,3 +39,41 @@ dir_out = mkpath(rel_path_merged)
 os.makedirs(dir_out, exist_ok=True)
 crop_name = os.path.join(dir_out, 'merged_image.jpg')
 cv2.imwrite(crop_name, img2)
+
+## dir_in = input_directory()
+#    img_path_1 = input_image(images_relpath=dir_in)
+#    img_path_2 = input_image(images_relpath=dir_in)
+    # Load image
+#    img1 = img_read(img_path_1)
+#   img2 = img_read(img_path_2)
+#   merge(dir_in, img1, img2)#
+
+def merge(dir_in, img1, img2):
+
+    img_path_1 = input_image(images_relpath=rel_path)
+    img_path_2 = input_image(images_relpath=rel_path)
+
+    # Load image
+    img1 = img_read(img_path_1)
+    img2 = img_read(img_path_2)
+
+    height, width = img1.shape[:2]
+    height2, width2 = img1.shape[:2]
+    assert (height == height2)
+    assert (width == width2)
+
+    half_width = width // 2
+
+    # Iterate in the range(begin, end)
+    for y in range(0, height):
+        img2[y, half_width:width] = img2[y, 0:half_width]
+        img2[y, 0:half_width]     = img1[y, half_width:width]
+
+    # Save
+    img_name_1, ext = os.path.splitext(os.path.basename(img_path_1))
+    img_name_2, _ = os.path.splitext(os.path.basename(img_path_2))
+
+    dir_out = mkpath(rel_path_merged)
+    os.makedirs(dir_out, exist_ok=True)
+    crop_name = os.path.join(dir_out, "merge-{}-{}.{}".format(img_name_1, img_name_2, ext))
+    cv2.imwrite(crop_name, img2)
--- python/puzzle/tools/image_manip.py
+++ python/puzzle/tools/image_manip.py
@@ -4,9 +4,9 @@ from PIL import Image
 from resizeimage import resizeimage
 
 
-def img_resize(img_path_in, img_path_out, size=1024):
+def img_resize(img_path_in, img_path_out, size=512):
     with open(img_path_in, 'rb') as fd_img:
         img = Image.open(fd_img)
-        img = resizeimage.resize_contain(img, [size, size])
+        img = resizeimage.resize_cover(img, [size, size])
         img = img.convert("RGB")
         img.save(img_path_out, img.format)
