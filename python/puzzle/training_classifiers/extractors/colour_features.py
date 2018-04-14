10697958e1f2acdd8f2aeb25ce73f84ef12de395
Alicia D <amd223@exeter.ac.uk>
2018-04-14 14:04:17 +0200

    Add skeleton for remaining extractors

---
+++ python/puzzle/training_classifiers/extractors/colour_features.py
@@ -0,0 +1,12 @@
+from puzzle.training_classifiers.extractors.feature_extractor import FeatureExtractor
+
+
+class ColourFeatureExtractor(FeatureExtractor):
+    def name(self):
+        return 'hist-colour'
+
+    def extract(self, img1, img2):
+        pass
+
+    def extract_feats_from_list(self, list):
+        pass
---
+++ python/puzzle/training_classifiers/extractors/l2_features.py
@@ -0,0 +1,12 @@
+from puzzle.training_classifiers.extractors.feature_extractor import FeatureExtractor
+
+
+class L2FeatureExtractor(FeatureExtractor):
+    def name(self):
+        return 'L2'
+
+    def extract(self, img1, img2):
+        pass
+
+    def extract_feats_from_list(self, list):
+        pass
